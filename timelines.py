2ef021d267246e73bfdfb25b6fa2523177d96ab0
JayDiddyThaGOAT <jalenjacksoncs@gmail.com>
2020-09-30 23:27:34 -0700

    Using PugSQL Instead of SqLite3, FlaskAPI Instead of Flask, & Added Foreman

--- timelines.py
+++ timelines.py
@@ -1,23 +1,7 @@
-import click
+from flask_api import FlaskAPI
 
-from flask import Flask, g
-from users import get_db
+import pugsql
+from users import queries
 
-app = Flask(__name__)
+app = FlaskAPI(__name__)
 app.config.from_envvar('APP_CONFIG')
-
-@app.teardown_appcontext
-def close_users_db(exception):
-    db = getattr(g, '_db', None)
-    if db is not None:
-        db.close()
-
-@app.cli.command("init")
-def init_db():
-    with app.app_context():
-        db = get_db()
-        with app.open_resource('schema.sql') as f:
-            db.cursor().executescript(f.read().decode("utf8"))
-        db.commit()
-    
-    click.echo('Initalized database')
--- users.py
+++ users.py
@@ -1,30 +1,16 @@
-import sqlite3
+from flask_api import FlaskAPI
+import pugsql
 
-import click
-from flask import Flask, g
-
-app = Flask(__name__)
+app = FlaskAPI(__name__)
 app.config.from_envvar('APP_CONFIG')
 
-def get_db():
-    db = getattr(g, '_db', None)
-    if db is None:
-        db = g._users_database = sqlite3.connect(app.config['DATABASE'], detect_types=sqlite3.PARSE_DECLTYPES)
-    
-    return db
+queries = pugsql.module('queries/')
+queries.connect(app.config['DATABASE_URL'])
 
-@app.teardown_appcontext
-def close_users_db(exception):
-    db = getattr(g, '_db', None)
-    if db is not None:
-        db.close()
-
-@app.cli.command("init")
+@app.cli.command('init')
 def init_db():
     with app.app_context():
-        db = get_db()
-        with app.open_resource('schema.sql') as f:
-            db.cursor().executescript(f.read().decode("utf8"))
+        db = queries.engine.raw_connection()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
         db.commit()
-    
-    click.echo('Initalized database')
